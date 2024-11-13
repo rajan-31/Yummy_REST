43bc9fd2e99e9b28df136824e70407846379aa76
Rajan Khade <rajankhade31@gmail.com>
2024-11-13 18:42:07 +0530

    init: basic config

--- src/main/java/org/myprojects/yummy_rest/YummyRestApplication.java
+++ src/main/java/org/myprojects/yummy_rest/YummyRestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YummyRestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YummyRestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YummyRestApplication.class, args);
+    }
 
 }
