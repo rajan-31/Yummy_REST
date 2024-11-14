b435c73a4c432059d037df905d98dd16d9e5be67
Rajan Khade <rajankhade31@gmail.com>
2024-11-15 00:41:59 +0530

    feat: add basic routes and configs

--- src/main/java/org/myprojects/yummy_rest/YummyRestApplication.java
+++ src/main/java/org/myprojects/yummy_rest/YummyRestApplication.java
@@ -2,12 +2,18 @@ package org.myprojects.yummy_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class YummyRestApplication {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(YummyRestApplication.class, args);
     }
-
 }
