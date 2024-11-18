c9e4b566b8de82dd9830604430cbee8c43bd509d
Rajan Khade <rajankhade31@gmail.com>
2024-11-18 21:51:49 +0530

    fix: use @Repository annotation

--- src/main/java/org/myprojects/yummy_rest/repo/CustomerRepo.java
+++ src/main/java/org/myprojects/yummy_rest/repo/CustomerRepo.java
@@ -2,10 +2,12 @@ package org.myprojects.yummy_rest.repo;
 
 import org.myprojects.yummy_rest.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
     Optional<Customer> findByEmail(String email);
     List<Customer> findByFirstName(String firstName);
